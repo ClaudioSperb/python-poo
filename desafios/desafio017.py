52c5cb6fcfe9d546d44c47ea8f187e7da2bc10cc
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-03 14:00:58 -0300

    Update desafio017.py

--- desafios/desafio017.py
+++ desafios/desafio017.py
@@ -6,12 +6,23 @@ from rich import print
 #CRIANDO ETIQUETAS
 
 class Produto:
+
+    """
+    Classe que adiciona um produto e um valor
+    """
+    
     def __init__(self, nome, preco):
         self.nome = nome
         self.preco = preco
 
 
     def etiqueta(self):
+
+        """
+        Metodo criado para criar uma etiqueta
+        :return: Uma etiqueta com o nome do Produto e o valor do produto
+        """
+
         real = f'R${self.preco:.2f}'.replace('_', '.').replace('.',',')
         caixa = Panel(f'[blue b]{self.nome:^32}[/] \n [green b]{29 * '~'}[/] \n[white b]{real:^32}[/]',title='PRODUTO',
                       width=35,style='red')
