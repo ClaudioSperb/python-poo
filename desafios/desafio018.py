86bcb7635922fad77ed8def81a3fa1e8a524c2aa
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-03 15:28:17 -0300

    Create desafio018.py

---
+++ desafios/desafio018.py
@@ -0,0 +1,30 @@
+from collections.abc import dict_values
+
+from rich.panel import Panel
+from rich import print
+
+#LISTA DE CHURRASCO
+#Consumo por pessoa = 400g
+#preço do Kg da Carne = $82.40/Kg
+
+class Churrasco:
+    def __init__(self, titulo, tot):
+        self.titulo = titulo
+        self.tot = tot
+
+    def analisar(self):
+        #Variaveis para os calculos
+        recomendacao = 0.4 * self.tot
+        custo_total = 82.40 * recomendacao
+        div_pessoa = custo_total / self.tot
+
+        caixa = Panel(f'Analisando [green b]{self.titulo}[/] com [blue b]{self.tot}[/] convidados\n'
+                      f'Cada convidado consumirá 0.4Kg e o preço por Kg é R$82.40\n'
+                      f'Recomendamos comprar [blue b]{recomendacao:.3f}Kg[/] de carne no total !\n'
+                      f'O valor total do Churrasco é de [green b]R${custo_total:.2f}[/] reais !\n'
+                      f'Valor por pessoa do Churrasco é [yellow b]R${div_pessoa:.2f}[/] reais!',
+                      title=self.titulo,width=100)
+        print(caixa)
+
+c1 = Churrasco('Churras dos Amigos', 500)
+c1.analisar()
