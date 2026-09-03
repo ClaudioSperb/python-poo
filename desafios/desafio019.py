f485cdf691822b3498d3431a4c8651d30928d9ad
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-03 16:16:24 -0300

    Update desafio019.py

---
+++ desafios/desafio019.py
@@ -0,0 +1,13 @@
+class Livro:
+    def __init__(self, nome, paginas):
+        self.nome = nome
+        self.pag = paginas
+
+    def avançar_pagina(self):
+        for c in range(self.pag):
+            print(f'{c + 1}', end=' ')
+            
+
+
+l1 = Livro('Teste', 5)
+l1.avançar_pagina()
