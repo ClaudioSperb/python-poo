81af95a01014a4cdc3783e7fd66194fc587229ce
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-04 13:08:37 -0300

    Atualização

--- desafios/desafio016.py
+++ desafios/desafio016.py
@@ -1,8 +1,13 @@
 from rich import print
+from rich import inspect
 
 #APRESENTAÇÃO FUNCIONÁRIO
 class Funcionario:
-    def __init__(self, nome, setor, cargo): #Método Construtor
+    #Atributo de Classe
+    empresa = 'Rek Reboques'
+
+    # Método Construtor
+    def __init__(self, nome, setor, cargo) -> str:
 
         #Atributos de instância
         self.nome = nome
@@ -10,12 +15,15 @@ class Funcionario:
         self.cargo = cargo
 
     def apresentação(self):
-        return f'🤝 Sou [green]{self.nome}[/], trabalho no setor [red]{self.setor}[/] e meu cargo é \n[green]{self.cargo}[/] na REK REBOQUES!'
+        return (f'🤝 Sou [green]{self.nome}[/], trabalho no setor [red]{self.setor}[/] e meu cargo é \n[green]'
+                f'{self.cargo}[/] na {Funcionario.empresa}')
 
 c1 = Funcionario('Claudio', 'Comercial', 'Gerente')
 print(c1.apresentação())
+inspect(c1)
 
 print('=-' * 35)
 
 c2 = Funcionario('Josiane', 'Recepção', 'Recepcionista')
 print(c2.apresentação())
+inspect(c2)
--- desafios/desafio019.py
+++ desafios/desafio019.py
@@ -1,13 +1,19 @@
+from time import sleep
+from rich import print
+
 class Livro:
     def __init__(self, nome, paginas):
         self.nome = nome
         self.pag = paginas
+        self.pagina_inicial = 1
 
-    def avançar_pagina(self):
+    def avançar_pagina(self, avancar):
+        tot_pag = avancar + self.pagina_inicial
         for c in range(self.pag):
-            print(f'{c + 1}', end=' ')
-        print(c + 1)
-
+            print(f'Pag{c + 1}➡️', end=' ')
+            sleep(0.5)
+        avancar += tot_pag
 
 l1 = Livro('Teste', 5)
-l1.avançar_pagina()
+l1.avançar_pagina(5)
+l1.avançar_pagina(2)
