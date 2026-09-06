c352d5731ccce1eae52ad23278656ef24b22b33d
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-06 19:27:50 -0300

    Update desafio021.py

--- desafios/desafio021/desafio021.py
+++ desafios/desafio021/desafio021.py
@@ -2,6 +2,8 @@ from rich import print
 
 class Caneta:
     def __init__(self, nome_cor):
+        self.destampado = True
+        self.quebra_linha = False
         self.cor = nome_cor.lower()
         if self.cor == 'vermelho':
             self.cor = 'red'
@@ -11,10 +13,25 @@ class Caneta:
             self.cor = 'blue'
 
     def escrever(self, msg):
-            print(f"[{self.cor}] {msg} [/]")
+        if self.destampado:
+            print(f"[b {self.cor}] {msg} [/]")
 
+    def quebrar(self, num=1):
+        self.quebra_linha = True
+        for i in range(num):
+            print('=-' * 30)
+
+    def destampar(self):
+        self.destampado = False
+        print(f'⚠️ [on red]A caneta {self.cor} esta tampada[/]')
 
 p1 = Caneta('Vermelho')
+p1.destampar()
 p1.escrever('Testando cores em PYTHON POO')
+p1.quebrar(5)
+
+p2 = Caneta('verde')
+p2.destampar()
+p2.escrever('Testando cores em PYTHON POO')
 
 
