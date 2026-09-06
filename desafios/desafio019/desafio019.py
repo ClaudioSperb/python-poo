720e95ed98f1d6c3bfdbc4970a8592a1839dd177
Claudio Sperb <claudiosperbpaulo@gmail.com>
2026-09-06 15:00:51 -0300

    Update desafio019.py

--- desafios/desafio019/desafio019.py
+++ desafios/desafio019/desafio019.py
@@ -2,18 +2,32 @@ from time import sleep
 from rich import print
 
 class Livro:
-    def __init__(self, nome, paginas):
-        self.nome = nome
-        self.pag = paginas
-        self.pagina_inicial = 1
+    def __init__(self, titulo, paginas):
+        self.titulo = titulo
+        self.total_paginas = paginas
+        self.pagina_atual = 1
 
-    def avançar_pagina(self, avancar):
-        tot_pag = avancar + self.pagina_inicial
-        for c in range(self.pag):
-            print(f'Pag{c + 1}➡️', end=' ')
-            sleep(0.5)
-        avancar += tot_pag
+        print(f":open_book: [blue]Você acabou de abrir o livro [red]'{self.titulo}'[/] que tem [green]{self.total_paginas}[/] paginas no total. Você agora está na [yellow]pagina {self.pagina_atual}[/yellow][/blue]")
 
-l1 = Livro('Teste', 5)
-l1.avançar_pagina(5)
-l1.avançar_pagina(2)
+    def avancar_paginas(self, qtd_paginas = 1):
+        cont = 0
+        for pg in range(0, qtd_paginas, 1):
+            if not self.fim_do_livro():
+                self.pagina_atual += 1
+                print(f"Pág{self.pagina_atual} :arrow_forward: ", end="")
+                sleep(0.3)
+                cont += 1
+        print(f"[blue]Você avançou {cont} paginas e esta na [yellow]página {self.pagina_atual}[/][/blue]")
+        if self.fim_do_livro():
+            print(f":closed_book: Você chegou no fim do Livro [green]'{self.titulo}'[/]:")
+
+    def fim_do_livro(self) -> bool:
+        if self.pagina_atual == self.total_paginas:
+            return True
+        else:
+            return False
+
+l1 = Livro('Livro 01', 20)
+l1.avancar_paginas(10)
+l1.avancar_paginas(5)
+l1.avancar_paginas(10)
